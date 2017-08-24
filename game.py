7f1d95ac896542eac7104db19e78ca6fc7dcb64e
Katie Byers <lobsterkatie@gmail.com>
2017-08-24 12:37:52 -0700

    added code to run a specified number of trials and compute stats for them

--- game.py
+++ game.py
@@ -1,6 +1,12 @@
 from data_structs import Stack, Queue
-from random import shuffle
+from random import shuffle, randint
 from datetime import datetime
+
+
+#get set of legal words
+with open("/usr/share/dict/words") as words_file:
+    LEGAL_WORDS = {line.strip() for line in words_file}
+
 
 
 class WordNode(object):
@@ -47,7 +53,7 @@ class GameRound(object):
         if len(start_word) != len(end_word):
             raise Exception("words must be the same length")
         else:
-            return super(GameRound, cls).__new__(cls, start_word, end_word)
+            return super(GameRound, cls).__new__(cls)
 
 
     def __init__(self, start_word, end_word):
@@ -55,29 +61,42 @@ class GameRound(object):
         self.end_word = WordNode(end_word)
         self.word_length = len(start_word)
 
-    def play_game(self):
-        """Play the game, recording DFS and BFS solution paths"""
 
+    def play_game(self, testing=False):
+        """Play the game, printing DFS and BFS solution paths to the terminal
+           if testing flag set to True.
+        """
 
         #do DFS, and time it
         start_time = datetime.now()
-        self.DFS_path = self._do_search("DFS")
+        self.DFS_path_end = self._do_search("DFS")
         end_time = datetime.now()
         self.DFS_time = end_time - start_time
 
         #do BFS, and time it
         start_time = datetime.now()
-        self.BFS_path = self._do_search("BFS")
+        self.BFS_path_end = self._do_search("BFS")
         end_time = datetime.now()
         self.BFS_time = end_time - start_time
 
-        #display the results
-        print "\nDFS\n"
-        self._print_path(self.DFS_path)
-        print self.DFS_time.microseconds / 1000.0, "ms"
-        print "\nBFS\n"
-        self._print_path(self.BFS_path)
-        print self.BFS_time.microseconds / 1000.0, "ms"
+        #get each path as a list of words
+        self.DFS_path = self._get_path(self.DFS_path_end)
+        self.BFS_path = self._get_path(self.BFS_path_end)
+
+        #if testing, display the results
+        if testing:
+            print "\nDFS\n"
+            print self.DFS_path
+            print self.DFS_time.microseconds / 1000.0, "ms"
+            print "\nBFS\n"
+            print self.BFS_path
+            print self.BFS_time.microseconds / 1000.0, "ms"
+
+        #return True/False to indicated path found or not
+        found = bool(self.DFS_path)
+        return found
+
+
 
 
 
@@ -144,6 +163,8 @@ class GameRound(object):
                     if (potential_new_word in LEGAL_WORDS and
                         potential_new_word not in used_words):
 
+                        # print potential_new_word
+
                         #if it's a new (and real) word, add to the set of
                         #already-used words, then create a WordNode out
                         #of it and add the WordNode to our stack or queue
@@ -170,14 +191,12 @@ class GameRound(object):
 
 
 
-    def _print_path(self, path_end):
-        """Print the path"""
+    def _get_path(self, path_end):
+        """Given the end of the path, return the path as a list of words"""
 
+        #if no path was found, return None
         if not path_end:
-            print "No path found"
-            return
-
-        #FIXME make this print out more nicely
+            return None
 
         #add all path nodes, in order, to a list
         path = []
@@ -190,20 +209,73 @@ class GameRound(object):
 
         #the nodes in the path point backwards, so we need to reverse our list
         #to have it display in the right direction
-        print path[::-1]
+        return path[::-1]
+
+
+
+
+def do_searches(start_word, end_word, num_trials):
+    """Do the requested number of trials and return a dictionary of the results
+    """
+
+    #make sure there is a path first - if not, return None
+    test = GameRound(start_word, end_word)
+    if not test.play_game():
+        return None
+
+    #now that we know there's at least one path between our start and end
+    #words, set things up to collect stats and then run the trials
+
+    DFS_times = []
+    DFS_path_lengths = []
+    BFS_times = []
+    BFS_path_lengths = []
+    sample_trial_num = randint(0, num_trials - 1)
+
+    # run the trials
+    for i in xrange(num_trials):
+        #for testing purposes, provide some sense of progress
+        # if i % 10 == 0:
+        #     print i
+
+        #do the search
+        trial = GameRound(start_word, end_word)
+        trial.play_game()
+
+        #record the results
+        DFS_times.append(trial.DFS_time.microseconds / 1000.0)
+        DFS_path_lengths.append(len(trial.DFS_path))
+        BFS_times.append(trial.BFS_time.microseconds / 1000.0)
+        BFS_path_lengths.append(len(trial.BFS_path))
+        if i == sample_trial_num:
+            sample_DFS_path = trial.DFS_path
+            sample_BFS_path = trial.BFS_path
+
+    #compute stats for this set of trials
+    avg_DFS_time = sum(DFS_times) / float(num_trials)
+    avg_DFS_path_length = sum(DFS_path_lengths) / float(num_trials)
+    avg_BFS_time = sum(BFS_times) / float(num_trials)
+    avg_BFS_path_length = sum(BFS_path_lengths) / float(num_trials)
+
+    #return the data in dictionary form
+    return {"avg_DFS_time": avg_DFS_time,
+            "avg_DFS_path_length": avg_DFS_path_length,
+            "sample_DFS_path": sample_DFS_path,
+            "avg_BFS_time": avg_BFS_time,
+            "avg_BFS_path_length": avg_BFS_path_length,
+            "sample_BFS_path": sample_BFS_path
+            }
 
 
 
 
 
 
-#get set of legal words
-with open("/usr/share/dict/words") as words_file:
-    LEGAL_WORDS = {line.strip() for line in words_file}
 
 
 x = GameRound("cat", "dog")
 x.play_game()
-x = GameRound("circle", "square")
-x.play_game()
+# x = GameRound("circle", "square")
+# x.play_game()
+
 
