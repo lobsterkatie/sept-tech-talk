4509dec01edeee4f061dc3ac465f633756b34c8f
Katie Byers <lobsterkatie@gmail.com>
2017-09-22 20:17:16 -0700

    added a bunch of false starts and, eventually, a chart to the homepage (layout still a hot mess); added chartJS code

--- static/js/doublets.js
+++ static/js/doublets.js
@@ -2,6 +2,7 @@
 
 $(document).ready(function () {
 
+    //do a search and add the results to the DOM
     $("#input-form").submit(function (evt) {
         evt.preventDefault();
         const formData = {"start_word": $("#start-word").val(),
@@ -19,6 +20,133 @@ $(document).ready(function () {
         });
     });
 
+
+    /////////////////////////////////////////////////////////////////////////
+    // ChartJS stuff
+
+    //reset the max-word-length dropdown (so it matches the default, specified
+    //below)
+    $("#max-word-length").val("5");
+
+    //set some global options for all charts
+    const dfsColor = "blue";
+    const bfsColor = "red";
+    Chart.defaults.global.elements.line.fill = false;
+
+    //the chart, chart data, and options are declared globally so they can be
+    //changed by various functions
+    let resultsChart;
+    let dataBySubject;
+    let currentSubject = "pathLength";
+    let currentMaxWordLength = 5;
+
+    let chartData = {
+        "labels": undefined, //will get set by AJAX call to get data
+        "datasets": [
+            {"label": "BFS",
+             "data": undefined, //will get set by AJAX call to get data
+             "borderColor": bfsColor,
+             "pointBackgroundColor": bfsColor,
+            },
+            {"label": "DFS",
+             "data": undefined, //will get set by AJAX call to get data
+             "borderColor": dfsColor,
+             "pointBackgroundColor": dfsColor,
+            }
+        ]
+    };
+
+    let chartOptions = {
+        "scales": {
+            "yAxes": [{
+                "ticks": {
+                    "beginAtZero": true
+                },
+                "scaleLabel": {
+                    "display": true,
+                    "labelString": "num words in path",
+                    "fontSize": 20,
+                }
+            }],
+            "xAxes": [{
+                "scaleLabel": {
+                    "display": true,
+                    "labelString": "word length",
+                    "fontSize": 20,
+                }
+            }]
+        },
+        "elements": {
+            "line": {
+                "cubicInterpolationMode": "monotone"
+            }
+        }
+    };
+
+
+    //get the correct data for the chart from global dataBySubject data store
+    function getData(subject, maxWordLength) {
+
+        //make a deep copy of the data for the given subject so we can mess
+        //with it without affecting the original
+        let data = JSON.parse(JSON.stringify(dataBySubject[subject]));
+
+        //slice the data for each search type so it only goes up through the
+        //desired word length
+        for (let searchType of ["BFS", "DFS"]) {
+            data[searchType] = data[searchType].slice(0, maxWordLength - 1);
+        }
+
+        //return the results
+        return data;
+    }
+
+
+    //create or update the chart
+    function makeChart() {
+        //get the data for the chart and put it in the appropriate places
+        //in the chartData and chartOptions objects
+        const data = getData(currentSubject, currentMaxWordLength);
+        chartData.datasets[0].data = data.BFS;
+        chartData.datasets[1].data = data.DFS;
+        chartOptions.scales.yAxes[0].scaleLabel.labelString = data.yAxisLabel;
+
+        //create (or recreate) the chart
+        const chartContext = $("#results-graph")[0].getContext('2d');
+        const chartConfig = {"type": "line",
+                             "data": chartData,
+                             "options": chartOptions};
+        resultsChart = new Chart(chartContext, chartConfig);
+    }
+
+
+    //make an AJAX request on pageload to get chart data from the database
+    $.get("/chart-data.json", function (results) {
+
+        //get x-axis labels
+        chartData.labels = results.wordLengths;
+
+        //put the  data into the global dataBySubject object for later
+        //reference
+        dataBySubject = results;
+
+        //make the chart
+        makeChart();
+    });
+
+
+    //change the max word length of the chart
+    $("#max-word-length").change(function (evt) {
+        currentMaxWordLength = $(this).val();
+        makeChart();
+    });
+
+
+    //change the subject of the chart
+    $(".swap-chart").click(function (evt) {
+        currentSubject = $(this).data("subject");
+        makeChart();
+    });
 
 });
 
